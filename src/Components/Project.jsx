92d7eb7d984d16b6d91d6061f4d8dbdf2f78572f
code-suhaib <suhaibstudy03@gmail.com>
2025-03-07 12:19:58 +0530

    bug fixing

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
         {/* Project 1 */}
         <div className="col-md-4">
           <div className="card">
-            <img src="spotify.png" className="card-img-top" alt="Spotify Clone" />
+            <img src="/spotify.png" className="card-img-top" alt="Spotify Clone" />
             <div className="card-body">
               <h5 className="card-title">Spotify Clone</h5>
               <p className="card-text">A Front-end Spotify clone.</p>
@@ -23,7 +23,7 @@ const Projects = () => {
         {/* Project 2 */}
         <div className="col-md-4">
           <div className="card">
-            <img src="jetsetgo.png" className="card-img-top" alt="JetsetGo Fullstack Webapp" />
+            <img src="/jetsetgo.png" className="card-img-top" alt="JetsetGo Fullstack Webapp" />
             <div className="card-body">
               <h5 className="card-title">JetsetGo - Fullstack Webapp</h5>
               <p className="card-text">A travel website made in React with a MongoDB backend.</p>
@@ -37,7 +37,7 @@ const Projects = () => {
         {/* Project 3 */}
         <div className="col-md-4">
           <div className="card">
-            <img src="coffee.png" className="card-img-top" alt="Responsive Coffee Site" />
+            <img src="/coffee.png" className="card-img-top" alt="Responsive Coffee Site" />
             <div className="card-body">
               <h5 className="card-title">Responsive Coffee Shop</h5>
               <p className="card-text">A responsive coffee shop website.</p>
